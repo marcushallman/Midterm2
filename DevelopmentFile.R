d10c0ecbf0755f49b1f35b9d52c5261f26064a67
Marcus Hallman <hallman@wustl.edu>
2018-03-19 23:15:05 -0500

    Prior
    added prior

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -70,6 +70,23 @@ function(object,theta){
 }
 )
 likelihood(new("rasch",name="fff",a=c(1,1,1,1,1,1),y=c(1,0,1,0,1,0)),2)
+
+
+#Prior
+setGeneric("prior", function(theta ="numeric"){   
+  standardGeneric("prior")
+})
+
+setMethod("prior", "rasch",
+function(theta){
+        return(dnorm(theta, mean=0, sd=3))      #returns the height of the normal curve evaluated at theta
+}
+)
+
+
+#
+
+
  #running it 
   
   current.code <- as.package("easyRasch")
