1f4718ed4719dd3e12800d5ade8d2c812183aba3
Marcus Hallman <hallman@wustl.edu>
2018-03-19 22:55:48 -0500

    made the likelihood function
    and made sure it all worked

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -43,22 +43,33 @@ setMethod("probability", "rasch",
 function(object,theta){
   exppart<-theta-sapply(object@a,exp)
  pij<<-exppart/(1-exppart)   #making Pij and creating the obhect in the global environment
- qij<<-1-pij                  #making qij
- pq<<- c(1:length(pij))       #cant think of a way to use apply here. this part makes an empty vector
+ qij<<-1-pij                  #making qij and creating the object in the global environment
+ pq<- c(1:length(pij))       #cant think of a way to use apply here. this part makes an empty vector
  for (i in 1:length(pij)){
    if (object@y[i]==1) pq[i]=pij[i]
-   else pq[i]=qij[i]                   #this part is and creating the pq object in the global environment
+   else pq[i]=qij[i]                   #this part is populating the pq object
  }
+ pq<<-pq   #this part creates the pq object in the global environment, because i can't think of a way to return both pij and pq, and the problem merely asks to 'create the outputs'
  print(pq)
  print(pij)
 }
 )
   
-fakea<-c(1,1,1,1,1,1,1,1) 
-fakey<-c(1,1,1,1,1,1,1,1)  
-blank<-new("rasch", name="Walt Whitman", a=c(1,1,1), y=c(1,0,1))
-probability(blank,2)
+
+#Likelihood
+
+setGeneric("likelihood", function(object="rasch",theta ="numeric"){   
+  standardGeneric("likelihood")
+})
+
+setMethod("likelihood", "rasch",
+function(object,theta){
+ probability(object,theta)   #running this code to make sure the right PQ is in the global environment
+ return(prod(pq))                   #prod on pq
   
+}
+)
+likelihood(new("rasch",name="fff",a=c(1,1,1,1,1,1),y=c(1,0,1,0,1,0)),2)
  #running it 
   
   current.code <- as.package("easyRasch")
