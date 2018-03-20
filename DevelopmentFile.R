4571b5c45c2b743bc8fedb9d1806ceb8f24b349c
Marcus Hallman <hallman@wustl.edu>
2018-03-19 21:51:18 -0500

    Initial commit

---
+++ DevelopmentFile.R
@@ -0,0 +1,47 @@
+## Load libraries and set working directory
+library(devtools)
+library(roxygen2)
+setwd("C:/Users/Marcus/Desktop/midterm") 
+
+#Make a class
+
+setClass(Class="rasch",
+         representation = representation(
+           name="character",
+           a="numeric",
+           y="numeric"
+         ),
+         prototype = prototype(
+           name = "",
+           a=c(),
+           y=c()
+         )
+)
+# A validity checker
+setValidity("rasch", function(object){
+  if(length(a)!=length(y)){return("question and answer vectors must have same length")}  #checks that the vectors are of same length
+})
+
+
+# A constructor
+setMethod("initialize", "rasch", function(.Object, ...){
+  value = callNextMethod()
+  validObject(value)
+  return(value)
+})
+
+  
+#Probability
+
+  
+  
+  
+  
+  
+  
+  
+ #running it 
+  
+  current.code <- as.package("squaresPack")
+  load_all(current.code)
+  document(current.code)  
